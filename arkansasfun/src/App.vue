cc75202bd4d76835856e7c6ac699ec40e9c0a084
Stephen Gray <sgraysk8er1@gmail.com>
2026-05-03 14:56:09 -0500

    Made sure everything is linked up properly, and added my images from figma. Need to add fav ico.

--- arkansasfun/src/App.vue
+++ arkansasfun/src/App.vue
@@ -1,12 +1,33 @@
 <script setup lang="ts">
+import { ref } from 'vue'
+
+const drawer = ref(false)
 </script>
 
 <template>
   <v-app>
+    <!-- Top bar -->
+    <v-app-bar color="primary" dark>
+      <v-app-bar-nav-icon @click="drawer = !drawer" />
+      <v-toolbar-title>My App</v-toolbar-title>
+
+      <!-- Quick links in top bar -->
+      <v-spacer />
+      <v-btn to="/" variant="text">Home</v-btn>
+      <v-btn to="/about" variant="text">About</v-btn>
+    </v-app-bar>
+
+    <!-- Side drawer -->
+    <v-navigation-drawer v-model="drawer" app>
+      <v-list>
+        <v-list-item to="/" title="Home" prepend-icon="mdi-home" />
+        <v-list-item to="/about" title="About" prepend-icon="mdi-information" />
+      </v-list>
+    </v-navigation-drawer>
+
+    <!-- Page content -->
     <v-main>
-      <v-container>
-        <v-btn color="primary">TS + Vuetify Works</v-btn>
-      </v-container>
+      <router-view />
     </v-main>
   </v-app>
 </template>
--- arkansasfun/src/main.ts
+++ arkansasfun/src/main.ts
@@ -1,13 +1,12 @@
-
 import { createApp } from "vue";
 import App from "./App.vue";
-
 
 import "vuetify/styles";
 import { createVuetify } from "vuetify";
 import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
 
+import { router } from "./router"; 
 
 import "@mdi/font/css/materialdesignicons.css";
 
@@ -16,4 +15,4 @@ const vuetify = createVuetify({
   directives,
 });
 
-createApp(App).use(vuetify).mount("#app");
+createApp(App).use(vuetify).use(router).mount("#app");
